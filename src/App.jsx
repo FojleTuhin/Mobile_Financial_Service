02e8e81b5e5c9dcb779daa312604700571e6a652
fojletuhin <fojletuhin@gmail.com>
2024-07-16 14:47:00 +0600

    git init

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+ 
+
+  return (
+   <h1 className="text-6xl">Mfs</h1>
+  )
+}
+
+export default App
---
+++ src/Root.jsx
@@ -0,0 +1,10 @@
+
+const Root = () => {
+    return (
+        <div>
+            I am from root
+        </div>
+    );
+};
+
+export default Root;
